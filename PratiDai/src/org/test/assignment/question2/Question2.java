fd2ef548a6b5803bcb58d4f3cacabc70eb224cd5
achyutneupane <achyutneupane@192.168.0.6>
2017-03-15 01:12:04 -0700

    question 2

---
+++ PratiDai/src/org/test/assignment/question2/Question2.java
@@ -0,0 +1,26 @@
+package org.test.assignment.question2;
+
+import java.util.Scanner;
+
+public class Question2 {
+
+	public static void main(String[] args) {
+		System.out.println("“Welcome to Cubing Program!");
+		System.out.println("\nPlease enter two positive whole numbers,");
+		System.out.println("\nstarting and finishing values");
+		System.out.println("\nfirst one should be smaller than the second one");
+
+		Scanner reader = new Scanner(System.in);  // Reading from System.in
+		System.out.println("\nEnter first number: \n");
+		int start = reader.nextInt(); 
+		
+		System.out.println("\nEnter second number: \n");
+		
+		int end = reader.nextInt(); 
+		
+		//Cube c = new Cube();
+		//c.processCube(start , end);
+		
+		System.out.println("\nGoodbye, thank you for using our program!");
+	}
+}
